b0be3a01a3e1b947ab00f72d327a5be58cbec991
Jarvis <jarvis_bot@srv1377835.hstgr.cloud>
2026-02-24 04:47:31 +0000

    Fix hip-api service discovery URI for Aspire HttpClient

--- HIP.Web/Program.cs
+++ HIP.Web/Program.cs
@@ -1,16 +1,22 @@
 using HIP.ServiceDefaults;
+using HIP.Web.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.AddServiceDefaults();
 
+builder.Services.AddRazorComponents();
+
 builder.Services.AddHttpClient("hip-api", client =>
 {
-    client.BaseAddress = new Uri("http://hip-api"); // security awareness: internal service DNS
+    client.BaseAddress = new Uri("https+http://hip-api");
 });
 
 var app = builder.Build();
 
-app.MapGet("/", () => Results.Ok(new { Name = "HIP Web", UtcTimestamp = DateTimeOffset.UtcNow }));
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>();
 app.MapDefaultEndpoints();
 
 app.Run();
